125083da2d67dfa499efde02a7084b927439bc72
SrJeffreyI <149419314+SrJeffreyI@users.noreply.github.com>
2026-04-18 13:24:38 -0400

    Delete device/xiaomi/serenity directory

--- device/xiaomi/serenity/twrp_serenity.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_arm64.mk)
-$(call inherit-product, device/xiaomi/serenity/device.mk)
-
-PRODUCT_DEVICE := serenity
-PRODUCT_NAME := twrp_serenity
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi A5
-PRODUCT_MANUFACTURER := xiaomi
